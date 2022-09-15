4b82abaf3565a618a0006e4d5455cfc4668c423d
Robert Rodrigues <robert.rm131@gmail.com>
2022-09-15 18:37:19 -0300

    FEAT<interfaces>:  export handler for interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+import { ICar } from './ICar';
+import { IModel } from './IModel';
+import { IService } from './IService';
+import { IVehicle } from './IVehicle';
+
+export { ICar, IModel, IService, IVehicle };
