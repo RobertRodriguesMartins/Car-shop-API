b9fad4b72aa49c474e17cc97b0d7aa6cfc196130
Robert Rodrigues <robert.rm131@gmail.com>
2022-09-15 19:07:20 -0300

    UPDATE<app>: using carRouter and errorMD

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMD from './middlewares/errorMD';
+import carRouter from './Routes/Car';
 
 const app = express();
+
+app.use(express.json());
+app.use(carRouter);
+app.use(errorMD);
 
 export default app;
